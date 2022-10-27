089ee578ccf01bba597e5c2756a899b38fa37ae3
Parvizkh8 <33002107+Parvizkh8@users.noreply.github.com>
2022-10-27 07:52:09 +0200

    add hero, services and contact sections to the home page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,26 @@ import heroBcg from '../assets/hero-bcg.jpeg'
 import heroBcg2 from '../assets/hero-bcg-2.jpeg'
 
 const Hero = () => {
-  return <h4>hero</h4>
+  return (
+    <Wrapper className='section-center'>
+      <article className='content'>
+        <h1>
+          design your <br />
+          comfort zone
+        </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Libero dolorum animi iure, accusamus doloribus quam.
+          Consequuntur ullam, sapiente voluptas, officia labore omnis unde
+          facilis hic libero reprehenderit fuga quibusdam quas!
+        </p>
+        <Link to='/products' className='btn hero-btn'>shop now</Link>
+      </article>
+      <article className='img-container'>
+        <img src={heroBcg} alt="nice table" className='main-img' />
+        <img src={heroBcg2} alt="person working" className='accent-img' />
+      </article>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.section`
