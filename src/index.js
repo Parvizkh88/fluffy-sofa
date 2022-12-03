4495dbbcd015a53ca9ca90ac42f3580aae627b58
Parvizkh8 <33002107+Parvizkh8@users.noreply.github.com>
2022-12-03 09:54:32 +0100

    auth0 setup - auth provider & useContext setup for authentication

--- src/index.js
+++ src/index.js
@@ -7,14 +7,25 @@ import { FilterProvider } from './context/filter_context'
 import { CartProvider } from './context/cart_context'
 import { UserProvider } from './context/user_context'
 import { Auth0Provider } from '@auth0/auth0-react'
+// dev-73j3qpz7te8eetxj.us.auth0.com
+// 7DDntq6OZJBJMgqcwZhKzq74m7xaqouD
 
 ReactDOM.render(
-    <ProductsProvider>
-        <FilterProvider>
-            <CartProvider>
-                <App />
-            </CartProvider>
-        </FilterProvider>
-    </ProductsProvider>,
+    <Auth0Provider
+        domain="dev-73j3qpz7te8eetxj.us.auth0.com"
+        clientId="zwejTUsGX6gsZ177By5P9OSxknLC0C2M"
+        redirectUri={window.location.origin}
+        cacheLocation='localstorage'
+    >
+        <UserProvider>
+            <ProductsProvider>
+                <FilterProvider>
+                    <CartProvider>
+                        <App />
+                    </CartProvider>
+                </FilterProvider>
+            </ProductsProvider>
+        </UserProvider>
+    </Auth0Provider>,
     document.getElementById('root')
 )
