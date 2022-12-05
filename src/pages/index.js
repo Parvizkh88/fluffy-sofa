127caf93ff38a1b46324708285ff2d230d783459
Parvizkh8 <33002107+Parvizkh8@users.noreply.github.com>
2022-12-05 07:22:29 +0100

    add authwrapper for prevent access to check out page for users who are not logged in

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,9 @@ import Cart from './CartPage';
 import Error from './ErrorPage';
 import Checkout from './CheckoutPage';
 import PrivateRoute from './PrivateRoute';
-
+import AuthWrapper from './AuthWrapper';
 
 export {
     Home, Products, SingleProduct, About, Cart,
-    Error, Checkout, PrivateRoute
+    Error, Checkout, PrivateRoute, AuthWrapper
 }
